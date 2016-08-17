79056d603cad20b27c459b7b401eda80c81616c4
Timothy Helton <timothy.j.helton@gmail.com>
2016-08-16 21:06:05 -0600

    Added derivative plot method

--- strumenti/plot.py
+++ strumenti/plot.py
@@ -36,20 +36,32 @@ class Derivative:
 
     Attributes:
 
+        - **dx**: *ndarray* derivative of x-axis data points
         - **dy**: *ndarray* derivative of y-axis data points
         - **ddy**: *ndarray* 2nd derivative of y-axis data points
+        - **plot_name**: *str* if non None value is supplied plot file will be \
+            saved
+        - **label_size**: *int* axis label font size
+        - **title_size**: *int* title font size
+        - **title**: *str* plot title
         - **x**: *ndarray* x-axis data points
+        - **x_label**: *str* x-axis label
         - **y**: *ndarray* y-axis data points
+        - **y_label**: *str* y-axis label
     """
     def __init__(self, x_points, y_points):
         self.x = x_points
         self.y = y_points
         self.dx = np.gradient(self.x)
-        self.dy = np.gradient(self.y) / self.dx
-        self.ddy = np.gradient(self.dy) / self.dx
+        self.dy = np.divide(np.gradient(self.y), self.dx)
+        self.ddy = np.divide(np.gradient(self.dy), self.dx)
+
         self.label_size = 16
         self.title_size = 20
         self.plot_name = None
+        self.title = 'Data\n'
+        self.x_label = 'X'
+        self.y_label = 'Y'
 
     def plot(self):
         """Generate plot of base data and derivatives."""
@@ -58,14 +70,16 @@ class Derivative:
 
         ax1.plot(self.x, self.y, '-b^', markersize=8, label='data')
         ax1.plot(self.x[1:-1], self.dy[1:-1], '-.ro', markersize=5,
-                 label='1st Derivative')
+                 label=r'$\mathbf{\frac{\delta y}{\delta x}}$')
         ax1.plot(self.x[2:-2], self.ddy[2:-2], '--md', markersize=5,
-                 label='2nd Derivative')
+                 label=r'$\mathbf{\frac{\delta^2 y}{\delta x^2}}$')
 
-        ax1.set_title('Data\n', fontsize=self.title_size, fontweight='bold')
-        ax1.legend(loc='upper right')
-        ax1.set_xlabel('X', fontsize=self.label_size, fontweight='bold')
-        ax1.set_ylabel('Y', fontsize=self.label_size, fontweight='bold')
+        ax1.set_title(self.title, fontsize=self.title_size, fontweight='bold')
+        ax1.legend(loc='upper right', fontsize=20)
+        ax1.set_xlabel(self.x_label, fontsize=self.label_size,
+                       fontweight='bold')
+        ax1.set_ylabel(self.y_label, fontsize=self.label_size,
+                       fontweight='bold')
         plt.grid(True)
 
         save_plot(name=self.plot_name)
