5174e67c142a5db0a7879302120eeff4c2cc6236
Timothy Helton <timothy.j.helton@gmail.com>
2017-02-14 07:04:19 -0700

    MAINT: update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 import strumenti
 
 
-with open('requests/__init__.py', 'r') as fd:
+with open('strumenti/__init__.py', 'r') as fd:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
                         fd.read(), re.MULTILINE).group(1)
 
